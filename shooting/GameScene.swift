8fee2f375f9af593b1a5e671cbc96b9b95bb07a4
hayashiyoshino <twinklestaryoshino@gmail.com>
2023-01-22 18:35:59 +0900

    feature: 衝突するよう設定

--- shooting/GameScene.swift
+++ shooting/GameScene.swift
@@ -19,10 +19,16 @@ class GameScene: SKScene {
     let motionMgr = CMMotionManager()
     var accelarationX: CGFloat = 0.0
     
+    let myShipCategory: UInt32 = 0b0001
+    let missileCategory: UInt32 = 0b0010
+    let enemyCategory: UInt32 = 0b0100
+    
     override func didMove(to view: SKView) {
         var sizeRate: CGFloat = 0.0
-        var myShipSize = CGSize(width: 0.0, height: 0.0)
+        var myShipSize = CGSize(width: 0.0, height: -1.0)
         let offsetY = frame.height / 20
+        
+        physicsWorld.gravity = CGVector(dx: 0, dy: 0)
         
         self.myShip = SKSpriteNode(imageNamed: "myShip")
         sizeRate = (frame.width / 5) / self.myShip.size.width
@@ -30,6 +36,10 @@ class GameScene: SKScene {
                             height: self.myShip.size.height * sizeRate)
         self.myShip.scale(to: myShipSize)
         self.myShip.position = CGPoint(x: 0, y: (-frame.height / 2) + offsetY + myShipSize.height / 2)
+        self.myShip.physicsBody = SKPhysicsBody(rectangleOf: self.myShip.size)
+        self.myShip.physicsBody?.categoryBitMask = self.myShipCategory
+        self.myShip.physicsBody?.collisionBitMask = self.enemyCategory
+        self.myShip.physicsBody?.isDynamic = true
         addChild(self.myShip)
         
         let tempEnemy = SKSpriteNode(imageNamed: "enemy1")
@@ -60,6 +70,10 @@ class GameScene: SKScene {
         let missilePos = CGPoint(x: self.myShip.position.x, y: self.myShip.position.y + (self.myShip.size.height / 2) - (missile.size.height / 2))
         
         missile.position = missilePos
+        missile.physicsBody = SKPhysicsBody(rectangleOf: missile.size)
+        missile.physicsBody?.categoryBitMask = self.missileCategory
+        missile.physicsBody?.collisionBitMask = self.enemyCategory
+        missile.physicsBody?.isDynamic = true
         addChild(missile)
         
         let move = SKAction.moveTo(y: frame.height + missile.size.height, duration: 0.5)
@@ -76,6 +90,9 @@ class GameScene: SKScene {
         enemy.scale(to: enemySize)
         let xPos = (frame.width / CGFloat.random(in: 1...5)) - frame.width / 2
         enemy.position = CGPoint(x: xPos, y: frame.height / 2)
+        enemy.physicsBody = SKPhysicsBody(rectangleOf: enemy.size)
+        enemy.physicsBody?.categoryBitMask = enemyCategory
+        enemy.physicsBody?.isDynamic = true
         addChild(enemy)
         
         let move = SKAction.moveTo(y: -frame.height / 2, duration: 2.0)
